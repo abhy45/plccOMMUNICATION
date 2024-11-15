afd2218acb898039f621add3e32bb551a82384cb
Abhishek Srivastava <abhysri76752@gmail.com>
2024-11-15 15:02:57 +0530

    Add project files.

---
+++ plccOMMUNICATION/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace plccOMMUNICATION
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblplcopen = new System.Windows.Forms.Label();
+            this.lblplcRead = new System.Windows.Forms.Label();
+            this.plcwrite = new System.Windows.Forms.Label();
+            this.lblError = new System.Windows.Forms.Label();
+            this.btnSethigh = new System.Windows.Forms.Button();
+            this.btnSetLow = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lblDataFromPLC = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblplcopen
+            // 
+            this.lblplcopen.Location = new System.Drawing.Point(57, 78);
+            this.lblplcopen.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblplcopen.Name = "lblplcopen";
+            this.lblplcopen.Size = new System.Drawing.Size(133, 28);
+            this.lblplcopen.TabIndex = 0;
+            this.lblplcopen.Text = "IsPLCConnect";
+            // 
+            // lblplcRead
+            // 
+            this.lblplcRead.Location = new System.Drawing.Point(301, 33);
+            this.lblplcRead.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblplcRead.Name = "lblplcRead";
+            this.lblplcRead.Size = new System.Drawing.Size(243, 53);
+            this.lblplcRead.TabIndex = 0;
+            this.lblplcRead.Text = "label1";
+            // 
+            // plcwrite
+            // 
+            this.plcwrite.Location = new System.Drawing.Point(663, 172);
+            this.plcwrite.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.plcwrite.Name = "plcwrite";
+            this.plcwrite.Size = new System.Drawing.Size(388, 52);
+            this.plcwrite.TabIndex = 0;
+            this.plcwrite.Text = "label1";
+            // 
+            // lblError
+            // 
+            this.lblError.Location = new System.Drawing.Point(57, 474);
+            this.lblError.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(144, 16);
+            this.lblError.TabIndex = 0;
+            this.lblError.Text = "SHOW ERROR";
+            // 
+            // btnSethigh
+            // 
+            this.btnSethigh.Location = new System.Drawing.Point(563, 404);
+            this.btnSethigh.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSethigh.Name = "btnSethigh";
+            this.btnSethigh.Size = new System.Drawing.Size(100, 28);
+            this.btnSethigh.TabIndex = 1;
+            this.btnSethigh.Text = "Set High";
+            this.btnSethigh.UseVisualStyleBackColor = true;
+            this.btnSethigh.Click += new System.EventHandler(this.btnSethigh_Click);
+            // 
+            // btnSetLow
+            // 
+            this.btnSetLow.Location = new System.Drawing.Point(732, 404);
+            this.btnSetLow.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSetLow.Name = "btnSetLow";
+            this.btnSetLow.Size = new System.Drawing.Size(100, 28);
+            this.btnSetLow.TabIndex = 1;
+            this.btnSetLow.Text = "Set Low";
+            this.btnSetLow.UseVisualStyleBackColor = true;
+            this.btnSetLow.Click += new System.EventHandler(this.btnSetLow_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(318, 404);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 28);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Send data PLC";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(80, 341);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(178, 37);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Read Data From Plc";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lblDataFromPLC
+            // 
+            this.lblDataFromPLC.Location = new System.Drawing.Point(90, 315);
+            this.lblDataFromPLC.Name = "lblDataFromPLC";
+            this.lblDataFromPLC.Size = new System.Drawing.Size(168, 23);
+            this.lblDataFromPLC.TabIndex = 4;
+            this.lblDataFromPLC.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.lblDataFromPLC);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnSetLow);
+            this.Controls.Add(this.btnSethigh);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.plcwrite);
+            this.Controls.Add(this.lblplcRead);
+            this.Controls.Add(this.lblplcopen);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblplcopen;
+        private System.Windows.Forms.Label lblplcRead;
+        private System.Windows.Forms.Label plcwrite;
+        private System.Windows.Forms.Label lblError;
+        private System.Windows.Forms.Button btnSethigh;
+        private System.Windows.Forms.Button btnSetLow;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lblDataFromPLC;
+    }
+}
+
---
+++ plccOMMUNICATION/Form1.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ActUtlTypeLib;
+
+namespace plccOMMUNICATION
+{
+    public partial class Form1 : Form
+    {
+        ActUtlType PLC;
+        Thread plcthread;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+
+            plcthread = new Thread(new ThreadStart(startPlCThread));
+            plcthread.Start();
+
+            plcthread = new Thread(new ThreadStart(ReadDataFromPLC));
+            plcthread.Start();
+
+            plcthread = new Thread(new ThreadStart(ReadDataFromPLC));
+            plcthread.Start();
+
+            plcthread = new Thread(new ThreadStart(ContinousOnFF));
+            plcthread.Start();
+        }
+
+        private void startPlCThread()
+        {
+            PLC = new ActUtlType();
+            PLC.ActLogicalStationNumber = 1;
+            start:
+            int IsPlcOpen = PLC.Open();//25202689
+
+            while (true)
+            {
+
+                Thread.Sleep(100);
+                if (IsPlcOpen == 0)
+                {
+                    lblplcopen.Text = "PlC Open";
+                    lblplcopen.BackColor = Color.LightGreen;
+
+                    ReadDataFromPLC();
+                   // WriteDataToPLC();
+                    if (IsPlcOpen != 0)
+                    {
+                        goto start;
+                    }
+                }
+                else
+                {
+                    lblplcopen.Text = "plc not open";
+                    lblplcopen.BackColor = Color.Red;
+                    goto start;
+                }
+
+            }
+
+        }
+
+        private void ReadDataFromPLC()
+        {
+            try
+            {
+                if (PLC != null)
+                {
+                  
+                    int dataValue = 0;
+                    int resultCode = PLC.GetDevice("M30", out dataValue);
+                  
+                        if (resultCode == 0)
+                        {
+                            lblplcRead.Text = "M30" + "-" + dataValue;
+                            lblplcRead.BackColor = Color.Green;
+                        }
+                        else
+                        {
+                            lblplcRead.Text = resultCode.ToString();
+                            lblplcRead.BackColor = Color.Red;
+                        }
+                  
+                }
+            }
+            catch (Exception ex)
+            {
+                lblError.Text = ex.Message;
+                lblError.BackColor = Color.Red;
+            }
+        }
+
+        private void WriteDataToPLC(bool setHigh)
+        {
+            try
+            {
+                if (PLC != null)
+                {
+                    string deviceName = "M10";
+                    int valueToSet = setHigh ? 1 : 0;
+
+                    int resultCode = PLC.SetDevice(deviceName, valueToSet);
+
+                        if (resultCode == 0)  // 0 means success
+                        {
+                            plcwrite.Text = "PLC write   :" + deviceName + "----" + valueToSet;
+                            plcwrite.BackColor = Color. Red;
+
+                        }
+                        else
+                        {
+                            plcwrite.BackColor = Color.Red;
+                            plcwrite.Text = resultCode.ToString();
+                        }
+                }
+            }
+            catch (Exception ex)
+            {
+                lblError.Text = ex.Message;
+                lblError.BackColor = Color.Red;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+           // plcthread.DisableComObjectEagerCleanup();
+
+        }
+        private void ReadDataFromPLCbackup()
+        {
+            try
+            {
+                if (PLC != null)
+                {
+                    string deviceName = "D100";
+                    int dataValue = 0;
+                    int resultCode = PLC.GetDevice(deviceName, out dataValue);
+                    this.Invoke(new Action(() =>
+                    {
+                        if (resultCode == 0)
+                        {
+                            lblplcRead.Text = "Read data" + deviceName + "" + dataValue;
+                            lblplcRead.BackColor = Color.Green;
+                        }
+                        else
+                        {
+                            lblplcRead.Text = resultCode.ToString();
+                            lblplcRead.BackColor = Color.Red;
+                        }
+                    }));
+                }
+            }
+            catch (Exception ex)
+            {
+                lblError.Text = ex.Message;
+                lblError.BackColor = Color.Red;
+            }
+        }
+
+        private void btnSethigh_Click(object sender, EventArgs e)
+        {
+            WriteDataToPLC(true);
+        }
+
+        private void btnSetLow_Click(object sender, EventArgs e)
+        {
+            WriteDataToPLC(false);
+        }
+        private void ContinousOnFF()
+        {
+            while (true)
+            {
+                Thread.Sleep(5000);
+                WriteDataToPLC(true);
+                Thread.Sleep(5000);
+                WriteDataToPLC(false);
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (PLC != null)
+                {
+                    string deviceName = "D30";
+                    int valueToSet = 500;
+
+                    int resultCode = PLC.SetDevice(deviceName, valueToSet);
+
+                    if (resultCode == 0)  // 0 means success
+                    {
+                        plcwrite.Text = "PLC write   :" + deviceName + "----" + valueToSet;
+                        plcwrite.BackColor = Color.Red;
+
+                    }
+                    else
+                    {
+                        plcwrite.BackColor = Color.Red;
+                        plcwrite.Text = resultCode.ToString();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                lblError.Text = ex.Message;
+                lblError.BackColor = Color.Red;
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (PLC != null)
+                {
+                    string deviceName = "D31";
+                    int dataValue = 0;
+                    int resultCode = PLC.GetDevice(deviceName, out dataValue);
+                    this.Invoke(new Action(() =>
+                    {
+                        if (resultCode == 0)
+                        {
+                            lblDataFromPLC.Text = "Read data : " + deviceName +"     " + dataValue;
+                            lblDataFromPLC.BackColor = Color.Green;
+                        }
+                        else
+                        {
+                            lblDataFromPLC.Text = resultCode.ToString();
+                            lblDataFromPLC.BackColor = Color.Red;
+                        }
+                    }));
+                }
+            }
+            catch (Exception ex)
+            {
+                lblError.Text = ex.Message;
+                lblError.BackColor = Color.Red;
+            }
+        }
+
+
+        //         try
+        //            {
+        //                string textToSend = "k100";
+        //        string baseDeviceName = "D30";
+
+        //                for (int i = 0; i<textToSend.Length; i++)
+        //                {
+        //                    string deviceName = baseDeviceName + i;  // Adjust based on your PLC’s addressing scheme
+        //        int asciiValue = (int)textToSend[i];
+
+        //        int resultCode = PLC.SetDevice(deviceName, asciiValue);
+
+        //                    if (resultCode != 0)
+        //                    {
+        //                        plcwrite.Text = $"Error writing {textToSend[i]} to {deviceName}. Error code: {resultCode}";
+        //                        plcwrite.BackColor = Color.Red;
+        //                        return;
+        //                    }
+        //}
+
+        //plcwrite.Text = "String sent successfully!";
+        //plcwrite.BackColor = Color.Green;
+        //            }
+        //            catch (Exception ex)
+        //            {
+        //                lblError.Text = ex.Message;
+        //lblError.BackColor = Color.Red;
+        //            }
+
+
+
+
+    }
+}
